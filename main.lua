d78033414df75b7fad880bf2d94a57f2b602e03a
ErickdeSouza <107515974+ErickdeSouza@users.noreply.github.com>
2023-12-15 19:40:21 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -14,33 +14,6 @@ local speed = 0.8
 
  
  
- 
-local c;
-local h;
-local bv;
-local bav;
-local cam;
-local flying;
-
- 
-function StartFly()
-    if not Client.Character or not Character.Head or flying then return end;
-    c = Character;
-    h = Humanoid;
-    h.PlatformStand = true;
-    cam = workspace:WaitForChild('Camera');
-    bv = Instance.new("BodyVelocity");
-    bav = Instance.new("BodyAngularVelocity");
-    bv.Velocity, bv.MaxForce, bv.P = Vector3.new(0, 0, 0), Vector3.new(10000, 10000, 10000), 1000;
-    bav.AngularVelocity, bav.MaxTorque, bav.P = Vector3.new(0, 0, 0), Vector3.new(10000, 10000, 10000), 1000;
-    bv.Parent = c.Head;
-    bav.Parent = c.Head;
-    h.Died:connect(function() flying = false end)
- 
-end
- 
- 
- 
 function movemodel(model,start,ennd,AddBy)
     local i = 0
  repeat
@@ -69,7 +42,7 @@ end
  
  
 while wait() do
-	local CoinContainer = Workspace:FindFirstChild("CoinContainer", true);
+    local CoinContainer = Workspace:FindFirstChild("CoinContainer", true);
     if CoinContainer and Client.PlayerGui.MainGUI.Lobby.Dock.CoinBag.Visible == true then
         local coin = CoinContainer:FindFirstChild("Coin_Server")
         
