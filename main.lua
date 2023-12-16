09e06ba4d9d6d6e5f022e0c2c204991e721791be
ErickdeSouza <107515974+ErickdeSouza@users.noreply.github.com>
2023-12-16 09:41:39 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -59,6 +59,7 @@ function movemodel(model, start, ennd, AddBy)
 
         local success, result = pcall(function()
             model:SetPrimaryPartCFrame(fds(start))
+	end)
 	start = sla
         if i >= 1 then
             break -- Encerra o movimento quando atinge o destino
