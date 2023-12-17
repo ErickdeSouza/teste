e7573434e34bfc8291877647b56df861b251cede
ErickdeSouza <107515974+ErickdeSouza@users.noreply.github.com>
2023-12-17 17:41:46 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -53,7 +53,7 @@ function movemodel(model, start, ennd, AddBy)
         i = math.clamp(i + (AddBy * x), 0, 1)
 
         local success, result = pcall(function()
-            model:SetPrimaryPartCFrame(RootPart.CFrame:Lerp(ennd, i))
+           Charcter.PrimaryPart.Cframe = start:Lerp(ennd, i))
         end)
 
         if not success then
