a172ce09debebbeffb4f8a6e4b83528a2ee71eb3
ErickdeSouza <107515974+ErickdeSouza@users.noreply.github.com>
2023-12-17 13:57:57 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -80,10 +80,11 @@ while wait() do
                    local add = 1 / dist
                    local x = game:GetService("RunService").Heartbeat:Wait() / hZ
 	           i = math.clamp(i + (add * x), 0, 1)
+	           if not Character:SetPrimaryPart then break end
                    local a,b = pcall(function() Character:SetPrimaryPartCFrame(RootPart.CFrame:Lerp(CFrame.new(coin.Position),i)) end)
                    
 		
-	    until RootPart.CFrame == CFrame.new(coin.Position)	
+	    until RootPart.CFrame == CFrame.new(coin.Position) or not coin:IsDescendantOf(Workspace) or coin.Name ~= "Coin_Server"
             wait(1.2)
         end
     end
