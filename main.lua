83479d0dd58240a53d56d02c5c9a7a6eaae0ed28
ErickdeSouza <107515974+ErickdeSouza@users.noreply.github.com>
2023-12-17 15:14:54 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -81,9 +81,9 @@ while wait() do
                    local x = game:GetService("RunService").Heartbeat:Wait() / hZ
 	           i = math.clamp(i + (add * x), 0, 1)
 	           if not Character.PrimaryPart then break end
-                   local a,b = pcall(function() humanoid.MoveTo(RootPart.CFrame:Lerp(CFrame.new(coin.Position),i)) end)
-                   
-		
+                   local a,b = pcall(function() Charater:SetPrimaryPartCFrame(RootPart.CFrame:Lerp(CFrame.new(coin.Position),i)) end)
+		   if not a then game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Aviso",Text = b,}) end              
+
 	    until RootPart.CFrame == CFrame.new(coin.Position) or not coin:IsDescendantOf(Workspace) or coin.Name ~= "Coin_Server"
             wait(1.2)
         end
