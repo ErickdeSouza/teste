a96725f78169f189635e609cf70014d8f67d1b0c
ErickdeSouza <107515974+ErickdeSouza@users.noreply.github.com>
2023-12-16 23:19:36 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -77,7 +77,7 @@ end
  
 while wait() do
 	local CoinContainer = Workspace:FindFirstChild("CoinContainer", true);
-    if CoinContainer and Client.PlayerGui.MainGUI.Lobby.Dock.CoinBag.Visible == true then
+    if Client.PlayerGui.MainGUI.Lobby.Dock.CoinBag.Visible == true then
         local coin = CoinContainer:FindFirstChild("Coin_Server")
         
         noclip()
